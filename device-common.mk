43b6bf6869b2afe33c790250c642f056a240026d
Aman Kumar <firelord.xda@gmail.com>
2016-05-19 14:39:06 +0530

    smdk5260: Add audio lib
    This reverts commit 8cb3ec69a8c662285652dff9fd7163591baf2866.

--- device-common.mk
+++ device-common.mk
@@ -34,6 +34,7 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
     audio.r_submix.default \
     audio.primary.default \
+    audio.primary.universal5260 \
     libaudio-resampler \
 
 PRODUCT_PROPERTY_OVERRIDES += \
