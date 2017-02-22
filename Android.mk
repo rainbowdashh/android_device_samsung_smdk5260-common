6d3ccc8753454828c7ca7c2e5a14e6287909b3de
FireLord1 <firelord.xda@gmail.com>
2017-02-22 23:01:49 +0530

    Revert "smdk5260: Add m2alte device"
    -no body joined for this device (samsung k zoom 3g or 4g)
    This reverts commit 73f6869bb5235ca724e02c19473a674cb63e5772.
    
    Change-Id: Ia1155cb511a38a00fce895bb5cde05bc43495df6
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter hl3g hllte m2alte, $(TARGET_DEVICE)),)
+ifneq ($(filter hl3g hllte, $(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
