f3858288364b30fd5bf45407c629caca2dde7369
Alex <razielrazi3@gmail.com>
2022-05-21 18:32:11 +0300

    readd camera id script
    altho we prolly don't need it

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -41,3 +41,10 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.universal5260.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.exynos.cam.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.exynos.cam.sh
+include $(BUILD_PREBUILT)
