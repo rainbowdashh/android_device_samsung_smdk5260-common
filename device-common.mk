9ee60bcbcdd3c0fc8d7f5ffd8a8dab7df3d0dc83
Alex <razielrazi3@gmail.com>
2022-07-04 19:43:40 +0300

    remove wpa supplicant

--- device-common.mk
+++ device-common.mk
@@ -236,6 +236,5 @@ PRODUCT_PACKAGES += \
     wifiloader
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
