fcuzzocrea/android_device_samsung_universal5420-common

universal5420: Make the external camera provider ignore internal cameras
* The external camera provider occupies our camera v4l2 nodes,
  potentially crashing the kernel driver and blocking the camera HAL.
  Unfortunately, there's no easy way to just disable it, so let's just
  tell it to ignore the internal video devices.

Change-Id: I00c67de1b94a1f7eca1bf458d1d0e4de3507aa81

device-common.mk
@@ -47,6 +47,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     camera.universal5420
 
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/camera/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-impl \

rainbowdashh/android_device_samsung_smdk5260-common

device-common.mk
@@ -69,6 +69,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl.exynos5260
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camera/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
 # Charger
 PRODUCT_PACKAGES += \
     charger \